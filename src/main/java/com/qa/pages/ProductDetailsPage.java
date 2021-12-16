56ecc5c6898e55bf5c12e6dbbac1f6d5fda96664
Pankaj Rode <pankaj.rode@ibm.com>
2021-12-17 00:31:04 +0530

    Code Commited To Assignment 4

--- src/main/java/com/qa/pages/ProductDetailsPage.java
+++ src/main/java/com/qa/pages/ProductDetailsPage.java
@@ -1,16 +1,28 @@
 package com.qa.pages;
 
 import com.qa.base.BaseTest;
+import com.qa.utils.TestUtils;
+
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.pagefactory.AndroidFindBy;
 
 public class ProductDetailsPage extends BaseTest {
+	TestUtils utils = new TestUtils();
 
-	@AndroidFindBy(xpath = "(//android.widget.TextView[@content-desc=\"test-Item title\"])[1]")
-	private MobileElement confirmProduct;
+	@AndroidFindBy(xpath = "//android.view.ViewGroup[@content-desc=\"test-Description\"]/android.widget.TextView[1]\n")
+	private MobileElement productName;
 
-	public String confirmProduct() {
-		String title = getText(confirmProduct);
-		return title;
+	@AndroidFindBy(accessibility = "test-BACK TO PRODUCTS")
+	private MobileElement backToProductsBtn;
+
+	public String getProductName() {
+		String text = getText(productName);
+		return text;
 	}
+
+	public ProductsPage pressBackToProductsBtn() {
+		click(backToProductsBtn);
+		return new ProductsPage();
+	}
+
 }
--- src/test/java/com/qa/test/ValidateProductTest.java
+++ src/test/java/com/qa/test/ValidateProductTest.java
@@ -2,21 +2,24 @@ package com.qa.test;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import com.qa.pages.*;
-import com.qa.base.BaseTest;
 
-public class LoginTest extends BaseTest {
+import com.qa.base.BaseTest;
+import com.qa.pages.LoginPage;
+import com.qa.pages.ProductDetailsPage;
+import com.qa.pages.ProductsPage;
+
+public class ValidateProductTest extends BaseTest {
 
 	@Test
-	public void Test() {
+	public void Login() {
 		LoginPage loginPage = new LoginPage(driver);
 		loginPage.enterUserName();
 		loginPage.enterPassword();
 
 		ProductsPage productsPage = loginPage.clickLoginBtn();
-		String actualProductTitle = productsPage.getTitle();
-		String expectedProductTitle = "PRODUCTS";
-		Assert.assertEquals(actualProductTitle, expectedProductTitle);
+		String actualTitle1 = productsPage.getTitle();
+		String expectedTitle1 = "PRODUCTS";
+		Assert.assertEquals(actualTitle1, expectedTitle1);
 
 		String actualProductName = productsPage.getProductName();
 		String expectedProductName = "Sauce Labs Backpack";
@@ -26,8 +29,9 @@ public class LoginTest extends BaseTest {
 		String expectedProductPrice = "$29.99";
 		Assert.assertEquals(actualProductPrice, expectedProductPrice);
 
-		ProductDetailsPage ProductDetailsPage = productsPage.clickProductTitle();
-		ProductDetailsPage.confirmProduct();
+		ProductDetailsPage productDetailsPage = productsPage.clickProductTitle();
+		productDetailsPage.getProductName();
+		productDetailsPage.pressBackToProductsBtn();
 
 	}
 
